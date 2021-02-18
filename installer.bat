d11b708fcd8f4f6f33f48ddb799496ea0d808cd2
Catover203 Creator <63941519+Catover203@users.noreply.github.com>
2021-02-18 18:24:33 +0700

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,7 +1,9 @@
 @echo off
-set /p wim_path="Install.wim file path: "
+set /p wim="Install.wim file path: "
 set /p deloy="Enter deloy path: "
 set /p index="Index: "
-set /p format="Need clean dir [1 = yes]: "
 
-if
+dism /apply-image /imagefile:%wim% /index:%index% /applydir:%deloy%
+
+echo Deloy install file to disk completed
+pause
